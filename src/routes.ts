fab9a2a626eb92277d2978519cc7774cf31372c8
cozette-cuzi <cuzicozette@gmail.com>
2022-03-02 16:54:54 +0100

    apply authentication on route

--- src/routes.ts
+++ src/routes.ts
@@ -4,13 +4,13 @@ import { createUserHandler } from './controllers/UserController';
 import validate from './middleware/ValidateResource';
 import { loginSchema } from './schema/LoginSchema';
 import { createUserSchema } from './schema/UserSchema';
-import { getBrreweriesHandler } from './controllers/BreweryController';
+import { getBreweriesHandler } from './controllers/BreweryController';
+import { requireAuth } from './middleware/requireAuth';
 
 const routes = (app: Express) => {
   app.post('/users', validate(createUserSchema), createUserHandler);
   app.post('/login', validate(loginSchema), loginHandler);
-
-  app.get('/breweries', getBrreweriesHandler);
+  app.get('/breweries',requireAuth , getBreweriesHandler);
 };
 
 export default routes;
