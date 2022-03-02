2aa8df462f4621ce3895485d0883b42131768ee9
cozette-cuzi <cuzicozette@gmail.com>
2022-03-02 01:53:59 +0100

    setting up

---
+++ config/custom-environment-variables.ts
@@ -0,0 +1,3 @@
+export default {
+  dbURI: "DB_URI"
+};
---
+++ src/utils/connect.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import config from "config";
+
+const connect = async () => {
+  const dbURI = config.get<string>("dbURI");
+  try {
+    await mongoose.connect(dbURI);
+  } catch (err) {
+    process.exit(1);
+  }
+};
+
+export default connect;
